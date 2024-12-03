3f2a78f9299fb18135deb604ca976e48c2a9a12b
Nadia Mostafa <Nadia.Mostafa1210@gmail.com>
2024-12-03 19:26:02 +0200

    Initial commit

---
+++ Assets/Code.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SeneManager : MonoBehaviour
+{
+
+
+    public void MyFunction()
+    {
+        Debug.Log("تم استدعاء الدالة MyFunction");
+    }
+    public void goToSene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+
+
+}
